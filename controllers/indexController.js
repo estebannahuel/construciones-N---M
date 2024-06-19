35eb7c3d42c8396dd6c91fd39e1644733471a847
Esteban Nahuel <estebanaugieri44@gmail.com>
2024-06-18 21:41:47 -0300

    feat: vista obras styles y Estructura

--- controllers/indexController.js
+++ controllers/indexController.js
@@ -1,15 +1,17 @@
-
+const obras = require('../data/obrasDurlock.json')
 
 module.exports = {
     index: (req,res) => {
    
-        return res.render('index'  )
+        return res.render('index')
     },
     servicios: (req,res) => {
         return res.render('servicios')
     },
     obrasRealizadas: (req,res) => {
-        return res.render('obraRealizadas')
+        return res.render('obraRealizadas',{
+            obras
+        })
     },
     
     contactos: (req,res) => {
